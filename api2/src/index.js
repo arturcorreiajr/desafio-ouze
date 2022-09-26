b4fe6cecb196881338da2536ee6c2307135f1364
arturcorreiajr <arturcorreiajr@gmail.com>
2022-09-25 23:01:24 -0300

    pipeline

--- api2/src/index.js
+++
@@ -1,7 +0,0 @@
-const app = require('express')();
-
-app.get('/api2', (req, res) => {
-  return res.send('Hello from Api 2');
-});
-
-app.listen('3002', () => console.log('Server running on port 3002'));
